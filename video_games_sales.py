cd02b771be93c862d10c69241f0a5a71f1bb45d5
Vitalii Mocanu <viemo777@gmail.com>
2022-12-07 23:04:08 +0200

    sec1

--- video_games_sales.py
+++ video_games_sales.py
@@ -20,4 +20,22 @@ print(vgs.loc[vgs['Name'] == 'Grand Theft Auto: Vice City']['Global_Sales'])
 
 
 ss = vgs['Global_Sales'].min()
+
 print(vgs.loc[vgs['Global_Sales'] == ss]['Name'])
+
+print('11. What is the average value of sales of all video games per genre in Japan?')
+print(vgs.groupby(['Genre'])['JP_Sales'].mean())
+
+print('12. How many unique names of video games in this dataframe?')
+print(vgs['Name'].nunique())
+
+print('13. Get the 3 most common genres of video games worldwide')
+print(vgs.groupby(['Genre'])['Global_Sales'].count().sort_values().head(3))
+
+print('14. How many video games have "super" word in their names?')
+my_np = vgs['Name'].unique()
+count = 0
+for x in my_np:
+    if 'super' in str(x.lower().split()):
+        count += 1
+print(count)
